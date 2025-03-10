b48065ce4f81aa08722a8746ece61039b563e3bf
Fillipe Goulart <fillipe.gsm@tutanota.com>
2025-03-10 15:12:06 -0300

    Ensure `tidy` plugin works with editorconfig

--- lua/custom/plugins/_tidy.lua
+++ lua/custom/plugins/_tidy.lua
@@ -1,5 +1,9 @@
 -- Remove whitespace on save
 return {
-    "mcauley-penney/tidy.nvim",
-    config = true,
+  'mcauley-penney/tidy.nvim',
+  config = true,
+  opts = {
+    -- Ensure the tidyng works even when an .editorconfig is in the project
+    provide_undefined_editorconfig_behavior = true,
+  },
 }
