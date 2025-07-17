47813b765c47a8b7c21f67c94873f5901efd54ea
Fillipe Goulart <fillipe.gsm@tutanota.com>
2025-07-17 12:30:43 -0300

    Remove `conform` duplication
    I just noticed this plugin was being configured twice, with two
    different configurations.
    
    This commit unifies both, and moves it to its own file because it is
    easier to manager.

--- lua/custom/plugins/_conform.lua
+++ lua/custom/plugins/_conform.lua
@@ -1,28 +1,40 @@
 -- Format code
 return {
   'stevearc/conform.nvim',
-  config = function()
-    require('conform').setup {
-      formatters_by_ft = {
-        lua = { 'stylua' },
-        python = {
-          -- Fix auto-fixable linter errors
-          -- "ruff_fix",
-          -- Run the formatter
-          'ruff_format',
-          -- Organize the imports
-          'ruff_organize_imports',
-        },
-        -- You can customize some of the format options for the filetype (:help conform.format)
-        -- rust = { 'rustfmt', lsp_format = 'fallback' },
-        -- Conform will run the first available formatter
-        javascript = { 'prettierd', 'prettier', stop_after_first = true },
+  event = { 'BufWritePre' },
+  cmd = { 'ConformInfo' },
+  keys = {
+    {
+      '<leader>f',
+      function()
+        require('conform').format { async = true, lsp_format = 'fallback' }
+      end,
+      mode = '',
+      desc = '[F]ormat buffer',
+    },
+  },
+  opts = {
+    notify_on_error = true,
+    format_on_save = {
+      -- These options will be passed to conform.format()
+      timeout_ms = 500,
+      lsp_format = 'never',
+    },
+
+    formatters_by_ft = {
+      lua = { 'stylua' },
+      python = {
+        -- Fix auto-fixable linter errors
+        -- "ruff_fix",
+        -- Run the formatter
+        'ruff_format',
+        -- Organize the imports
+        'ruff_organize_imports',
       },
-      format_on_save = {
-        -- These options will be passed to conform.format()
-        timeout_ms = 500,
-        lsp_format = 'never',
-      },
-    }
-  end,
+      -- You can customize some of the format options for the filetype (:help conform.format)
+      -- rust = { 'rustfmt', lsp_format = 'fallback' },
+      -- Conform will run the first available formatter
+      javascript = { 'prettierd', 'prettier', stop_after_first = true },
+    },
+  },
 }
