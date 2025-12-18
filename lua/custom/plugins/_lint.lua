7479222440d99e55be70f8d3fcb4d86f0adcfe6b
Fillipe Goulart <fillipe.goulart@optibus.com>
2025-12-18 14:10:45 -0300

    Add languagetool as linter for asciidoc
    The languagetool is a separate tool for checking grammar.
    
    It installs separately, but with help of Claude I managed to plug it
    into the linter.

--- lua/custom/plugins/_lint.lua
+++ lua/custom/plugins/_lint.lua
@@ -2,13 +2,57 @@
 return {
   'mfussenegger/nvim-lint',
   config = function()
-    require('lint').linters_by_ft = {
+    local lint = require 'lint'
+
+    -- Custom LanguageTool linter
+    -- Requires `languagetool-commandline` installed and in the $HOME directory
+    -- Follow https://github.com/languagetool-org/languagetool
+    lint.linters.languagetool = {
+      name = 'languagetool',
+      cmd = 'java',
+      stdin = false,
+      args = {
+        '-jar',
+        vim.fn.expand '~/LanguageTool-6.6-stable/languagetool-commandline.jar',
+        '-l',
+        'en-US',
+      },
+      stream = 'stdout',
+      ignore_exitcode = true,
+      parser = function(output, bufnr)
+        local diagnostics = {}
+        -- Parse LanguageTool output line by line
+        -- Format: "Line X, column Y, Rule ID: ... Message: ..."
+        for line_text in output:gmatch '[^\r\n]+' do
+          local line, col = line_text:match 'Line (%d+), column (%d+),'
+          if line and col then
+            -- Next line should have "Message: ..."
+            local message = output:match('Line ' .. line .. ', column ' .. col .. '.-\nMessage: ([^\n]+)')
+            if message then
+              table.insert(diagnostics, {
+                lnum = tonumber(line) - 1,
+                col = tonumber(col) - 1,
+                end_lnum = tonumber(line) - 1,
+                end_col = tonumber(col),
+                severity = vim.diagnostic.severity.WARN,
+                message = message,
+                source = 'LanguageTool',
+              })
+            end
+          end
+        end
+        return diagnostics
+      end,
+    }
+
+    lint.linters_by_ft = {
       python = { 'ruff' },
       sh = { 'shellcheck' },
       javascript = { 'eslint_d' },
       json = { 'jsonlint' },
       markdown = { 'markdownlint' },
       tex = { 'chktex' },
+      asciidoc = { 'languagetool' },
     }
 
     vim.api.nvim_create_autocmd({ 'BufWritePost' }, {
